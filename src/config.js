42d4ce8651ca4834c723f8be1d25700eb5e97f98
Jerrad Moon <jerradm85@gmail.com>
2020-09-23 20:07:50 -0700

    mvp done

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: 'https://floating-dawn-53690.herokuapp.com/api',
     TOKEN_KEY: 'thingful-client-auth-token',
   }
